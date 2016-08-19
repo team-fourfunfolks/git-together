11f77ba07a955d3a4e3da68d992e900b922dc7d5
Colin Brownlie <colin@Colins-MacBook-Pro.local>
2016-08-19 10:20:35 -0700

    committing terminal.js and ipc.js

--- src/ipc.js
+++ src/ipc.js
@@ -4,4 +4,7 @@ const ipcRenderer = require ('electron').ipcRenderer;
 const ipcMain = require ('electron').ipcMain;
 const Terminal = require('xterm');
 const localGit = {test: 'testing'};
+// const pty = require('pty.js');
+//const exec = require('child-process');
+//const fork = require('child-process').fork;
 //module.exports = {renderer: ipcRenderer}
--- src/terminal.js
+++ src/terminal.js
@@ -17,27 +17,43 @@ class Term extends Component {
 			term.write(arg)
 		})
 
-		term.on('input', function(input) {
-			ipcRenderer.send('term-input', input)
-		})
-		
-		ipcRenderer.on('reply', (event, arg) => {
-			term.write(arg)
-		})
-		
 		term.prompt =  () => {
-    	term.write('\r\n' + '$ ');
-  	};
+    		term.write('\r\n' + '$ ');
+  		};
+		// term.on('data', function(input) {
+		// 	ipcRenderer.send('term-input', input)
+		// })
+		
+
+
 		term.prompt();
+
+		let str = '';
+
+		term.on('data', function(input) {
+			str += input;
+		})
+
+		ipcRenderer.on('reply', (event, arg) => {
+			term.write(arg);
+			term.prompt();
+		})
+
 
 		term.on('key', function(key, ev) {
 			var printable = (
 			!ev.altKey && !ev.altGraphKey && !ev.ctrlKey && !ev.metaKey
 			);
-			if (ev.keyCode === 13) term.prompt();
+			if (ev.keyCode === 13) {
+				term.prompt();
+				console.log(str)
+				
+				ipcRenderer.send('term-input', str)
+				str = ''				
+			}
       else if (ev.keyCode === 8) {
-			if (term.x > 2) {
-			term.write('\b \b');
+				if (term.x > 2) {
+				term.write('\b \b');
 			}
 			} else if (printable) {
 			term.write(key);
